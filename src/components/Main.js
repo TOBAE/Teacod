a0f87659338d43c56b0789cabf711df4834d326a
unknown <Assumpta>
2022-08-05 21:06:27 +0100

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+export default function Main () {
+    return (
+        <>
+       
+        <div className='main'>
+                
+            <div className='text_container'>
+
+                <div className='main_text'>
+
+                 <p className='what'>what</p>
+                 <h1 className='main_heading'>would you like to <strong>order</strong> today?</h1>
+                 <p className='main_para'>
+                     Our job is to filling your tummy with delicious food
+                     with fast and free delivery.
+                 </p>
+
+                </div>
+
+                <div className='btn'>
+                  <button className='btn1'>free delivery</button>
+                  <button className='btn2'>dine in</button>
+                </div>
+
+            </div>
+
+            <div className='image_container'>
+                <img src='../images/grilled_chicken.jpg' alt=''/>
+            </div>
+              
+        </div>
+        </>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <>
+    
+    <nav className='nav'>
+
+    <div className='logo'>
+      <h1><span className='logo_span'>t</span>eacod</h1>
+    </div>
+
+    <ul className='nav_items'>
+      <li>home</li>
+      <li>menu</li>
+      <li>about us</li>
+      <li>contact</li>
+    </ul>
+
+    <div className="cart">
+      <div className='cart_item'>
+         <i class="fa fa-search"></i>
+      </div>
+
+      <div className='cart_item'>
+         <i class="fa fa-cart-plus"></i>
+      </div>
+
+      <div className='cart_item'>
+          <p className='log_text'>log in</p>
+      </div>
+      
+    </div>
+
+    </nav>
+
+    </>
+  )
+}
+
+export default Navbar
